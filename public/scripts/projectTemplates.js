36bcb6da2a52c9b9b8cd27d7eb18f5edbfafa6cd
Trey64 <thomasellison.r@gmail.com>
2017-08-23 18:07:49 -0700

    Put JS files in scripts folder and put all files inside a public/ directory

---
+++ public/scripts/projectTemplates.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var rawData = [
+  {
+    title: 'Battleship',
+    screenshot: '<img class="project-image" src="http://via.placeholder.com/200x200" />',
+    projectUrl: 'http://github.com/Trey64/battleship',
+    description: 'This was my 201 project'
+  }
+];
