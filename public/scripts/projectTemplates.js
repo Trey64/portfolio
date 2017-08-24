c985b20ed534a04ee2c739f3f6a4e67b32b684f7
Trey64 <thomasellison.r@gmail.com>
2017-08-24 07:13:58 -0700

    added server.js and package.json

--- public/scripts/projectTemplates.js
+++ public/scripts/projectTemplates.js
@@ -3,7 +3,7 @@
 var rawData = [
   {
     title: 'Battleship',
-    screenshot: '<img class="project-image" src="http://via.placeholder.com/200x200" />',
+    screenshot: 'http://via.placeholder.com/200x200',
     projectUrl: 'http://github.com/Trey64/battleship',
     description: 'This was my 201 project'
   }
---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// DONE: Initialize your project using NPM to create and populate a package.json file
+
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
+// Remember to install express, and be sure that it's been added to your package.json as a dependency
+const express = require('express');
+const app = express();
+
+// There is also a package here called body-parser, which is required in for use in a new route.
+// Be sure to install that and save it as a dependency after you create your package.json.
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = process.env.PORT || 3000;
+
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
+
+
+app.listen(PORT, function() {
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log('Our app is listening on port ' + PORT);
+});
