3940845158c0ce50aa0b93f9843cf2fb11e1f37c
Trey64 <thomasellison.r@gmail.com>
2017-08-29 17:07:22 -0700

    Added repo.js

---
+++ public/scripts/models/repo.js
@@ -0,0 +1,24 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      method: 'GET',
+      headers: {
+        Authorization: `token ${githubToken}` //eslint-disable-line
+      }
+    })
+    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    .then(callback);
+  };
+
+  repos.with = attr => repos.all.filter(repo => repo[attr]);
+
+  module.repos = repos;
+})(app);
