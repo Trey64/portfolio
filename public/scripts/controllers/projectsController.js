79b0a77916ad53ae86d62b473de1d231918e9e1a
Trey64 <thomasellison.r@gmail.com>
2017-09-04 23:47:02 -0700

    uncommented projectsController

--- public/scripts/controllers/projectsController.js
+++ public/scripts/controllers/projectsController.js
@@ -6,6 +6,7 @@ var app = app || {};
   const projectsController = {};
 
   projectsController.init = () => {
+    $('.tab-content').hide();
     $('#projects').show();
     app.repos.requestRepos(app.repoView.index);
   }
