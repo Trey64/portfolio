c13489298f968a6b6731c08a4d4e9fb0bc8a32f4
Trey64 <thomasellison.r@gmail.com>
2017-09-04 23:43:42 -0700

    uncommented portfolioView.handleTabNav()

--- public/scripts/views/portfolioView.js
+++ public/scripts/views/portfolioView.js
@@ -7,6 +7,14 @@ const portfolioView = {};
 $('#projects').hide();
 $('#about').hide();
 
+portfolioView.handleTabNav = function() {
+  $('.main-nav .tab').on('click', function() {
+    var $where = $(this).data('content');
+    $('.tab-content').hide();
+    $('#' + $where).fadeIn(1000);
+  });
+};
+
 portfolioView.handleCarousel = function() {
   var carouselSlider = setInterval(function(){
     $('.carousel ul').animate({marginLeft:-1440},1000,function(){
